e1bede90a709f8056266d7edf6081ed151a05367
qasim <qasim86922@gmail.com>
2020-04-03 02:37:14 +0500

    db uri update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  secret: 'MY SECRET',
+  secret: 'Developed by Qasim Mahmood',
   DB_URI_DEV: 'mongodb://mongodb0.example.com:27017/admin',
   DB_URI: 'mongodb://mongodb0.example.com:27017/admin'
 };
